30209daced94a02ab3458dfdf7406c373c8e34f0
bwodhams <bpwodhams@gmail.com>
2018-11-18 12:25:24 -0700

    graph 1 and 2 fully working

--- Project_2/script.js
+++ Project_2/script.js
@@ -13,63 +13,63 @@ function graph1() {
     numEnterprise = (result.match(/Enterprise/g) || []).length;
     numEnterprise = (result.match(/Enterpise/g) || []).length;
     numContent = (result.match(/Content/g) || []).length;
-    createGraph(numTransit, numEnterprise, numContent);
-
-    function createGraph(transit, enterprise, content) {
-        var ctx = document.getElementById("myChart").getContext('2d');
-        Chart.defaults.global.plugins.datalabels.font.size = 30;
-        var data = [{
-            data: [transit, enterprise, content],
-            backgroundColor: [
-                "#4D4D4D",
-                "#5DA5DA",
-                "#FAA43A"
-            ],
-            borderColor: 'black',
-            label: "AS Classification",
-        }];
-
-        var options = {
-            responsive: true,
-            legend: {
-                display: true
-            },
-            title: {
-                display: true,
-                text: 'AS Classification'
-            },
-            plugins: {
-                datalabels: {
-                    formatter: (value, ctx) => {
-
-                        let sum = 0;
-                        let dataArr = ctx.chart.data.datasets[0].data;
-                        dataArr.map(data => {
-                            sum += data;
-                        });
-                        let percentage = (value * 100 / sum).toFixed(2) + "%";
-                        return percentage;
-
-
-                    },
-                    fontsize: 30,
-                    color: '#ffffff',
-
-                }
-            }
-        };
-
-
-        var myChart = new Chart(ctx, {
-            type: 'pie',
-            data: {
-                datasets: data,
-                labels: ['Transit/Access', 'Enterprise', 'Content']
-            },
-            options: options
-        });
-    }
+    createGraph_1(numTransit, numEnterprise, numContent);
 }
+function createGraph_1(transit, enterprise, content) {
+    var ctx = document.getElementById("graph1").getContext('2d');
+    Chart.defaults.global.plugins.datalabels.font.size = 30;
+    var data = [{
+        data: [transit, enterprise, content],
+        backgroundColor: [
+            "#4D4D4D",
+            "#5DA5DA",
+            "#FAA43A"
+        ],
+        borderColor: 'black',
+        label: "AS Classification",
+    }];
+
+    var options = {
+        responsive: true,
+        legend: {
+            display: true
+        },
+        title: {
+            display: true,
+            text: 'AS Classification'
+        },
+        plugins: {
+            datalabels: {
+                formatter: (value, ctx) => {
+
+                    let sum = 0;
+                    let dataArr = ctx.chart.data.datasets[0].data;
+                    dataArr.map(data => {
+                        sum += data;
+                    });
+                    let percentage = (value * 100 / sum).toFixed(2) + "%";
+                    return percentage;
+
+
+                },
+                fontsize: 30,
+                color: '#ffffff',
+
+            }
+        }
+    };
+
+
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            datasets: data,
+            labels: ['Transit/Access', 'Enterprise', 'Content']
+        },
+        options: options
+    });
+}
+
 
 let bin1 = 0;
 let bin2_5 = 0;
@@ -112,19 +112,34 @@ function graph2() {
 function graph2_p2p() {
     console.log("in graph p2p");
     console.log("still in p2p");
+    let compVals = [];
+    for(let j = 0; j < p2p.length; j++){
+        compVals.push(parseInt((p2p[j].substring(0, p2p[j].indexOf('|', p2p[j].indexOf('|') + 1))).replace('|', '')));
+    }
+    compVals.sort(function(a,b){return a - b});
     for (let i = 0; i < p2p.length; i++) {
         let startIndex = 0;
         let endIndex = p2p[i].indexOf('|', p2p[i].indexOf('|') + 1);
         let reverse = p2p[i].substring(p2p[i].indexOf('|') + 1, p2p[i].indexOf('|', p2p[i].indexOf('|') + 1)) + '|' + p2p[i].substring(0, p2p[i].indexOf('|'));
+        
+        //now that it's sorted, we will remove bar from p2p forward val, to compare with comp vals and reversed
+        //vals.  do fast search to find appropriate value to search for, instead of 
+        //linear search.  example, half way through list, see if less than or greater, then halve and h alve
+        //etc etc
+        p2pForward.push(p2p[i].substring(0, endIndex));
+        /*
         if (p2pForward.indexOf(p2p[i].substring(0, endIndex)) > -1) {
             //string already exists
+            console.log("normal string exists");
         } else {
             if (p2pForward.indexOf(reverse) > -1) {
                 //reverse p2p exists
+                console.log("reverse p2p exists");
             } else {
                 p2pForward.push(p2p[i].substring(0, endIndex));
             }
         }
+        */
         //p2pReverse.push(p2p[i].substring(p2p[i].indexOf('|') + 1, p2p[i].indexOf('|', p2p[i].indexOf('|') + 1)) + '|' + p2p[i].substring(0, p2p[i].indexOf('|')));
     }
     console.log("here 1");
@@ -210,4 +225,54 @@ function graph2_print() {
     console.log("bin 100 - 200 = " + bin100_200);
     console.log("bin 200 - 1000 = " + bin200_1000);
     console.log("bin 1000+ = " + bin1000_);
+    createGraph_2(bin1, bin2_5, bin5_100, bin100_200, bin200_1000, bin1000_);
+}
+
+function createGraph_2(bin1, bin2_5, bin5_100, bin100_200, bin200_1000, bin1000_) {
+    var ctx = document.getElementById("graph2").getContext('2d');
+    Chart.defaults.global.plugins.datalabels.font.size = 30;
+    var data = [{
+        data: [bin1, bin2_5, bin5_100, bin100_200, bin200_1000, bin1000_],
+        backgroundColor: [
+            "#ffffff",
+            "#5DA5DA",
+            "#FAA43A",
+            "#B3E5FC",
+            "#4FC3F7",
+            "#03A9F4"
+
+        ],
+        borderColor: 'black',
+        label: "Bin Distribution",
+    }];
+
+    var options = {
+        responsive: true,
+        legend: {
+            display: true
+        },
+        title: {
+            display: true,
+            text: 'AS Node Degree Distribution'
+        },
+        scales:{
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 8602,
+                }
+            }]
+        }
+        
+    };
+
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            datasets: data,
+            labels: ['Bin 1', 'Bin 2 - 5', 'Bin 5 - 100', 'Bin 100 - 200', 'Bin 200 - 1000', 'Bin > 1000']
+        },
+        options: options
+    });
 }
